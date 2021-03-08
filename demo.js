6006deb31ddcf0192a5f468a6c45e11298850a69
steve zhang <871506263@qq.com>
2021-03-08 18:41:40 +0800

    first commit

---
+++ demo.js
@@ -0,0 +1,5 @@
+const obj = { a: 1 };
+export const obj1 = Object.assign({},obj);
+
+const obj2 = { b: 2 };
+export default obj2;
---
+++ index.js
@@ -0,0 +1,4 @@
+import obj2, { obj1 } from './demo';
+
+console.log('obj1 :>> ', obj1);
+console.log('obj2 :>> ', obj2);
