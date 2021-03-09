e0fe447506c740ab33756da0eabb2a45cd94d8b2
steve zhang <871506263@qq.com>
2021-03-09 10:28:21 +0800

    提交代码于: 2021-03-09 10:28:21

---
+++ _demo.js
@@ -0,0 +1,16 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = exports.obj1 = void 0;
+var obj = {
+  a: 1
+};
+var obj1 = Object.assign({}, obj);
+exports.obj1 = obj1;
+var obj2 = {
+  b: 2
+};
+var _default = obj2;
+exports.default = _default;
