653bec383d0a7b593ccd48ca06fd4b731adc8b02
steve zhang <871506263@qq.com>
2021-03-10 02:56:39 +0800

    提交代码于: 2021-03-10 02:56:39

--- _demo.js
+++ _demo.js
@@ -3,7 +3,8 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = exports.obj1 = void 0;
+console.log('exports 000:>> ', exports);
+exports["default"] = exports.obj1 = void 0;
 var obj = {
   a: 1
 };
@@ -13,4 +14,8 @@ var obj2 = {
   b: 2
 };
 var _default = obj2;
-exports.default = _default;
+exports["default"] = _default;
+console.log('exports 001:>> ', exports);
+console.log('module.exports :>> ', module.exports);
+console.log('exports.__esModule :>> ', exports.__esModule);
+console.log('module.exports.__esModule :>> ', module.exports.__esModule);
